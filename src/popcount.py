672f0c0be6a286ac1cb86b9692899741e926b1ca
hppRC <hpp.ricecake@gmail.com>
2020-08-06 13:55:45 +0900

    🥺chore

--- src/popcount.py
+++ src/popcount.py
@@ -1,12 +1,17 @@
 import time
-import sys
 from typing import Callable
+
+def make_bincountlist(x: int) -> None:
+    res = [0]
+    for _ in range(x.bit_length()):
+        res += [j+1 for j in res]
+
 
 def popcount_while(x: int) -> int:
     ret = 0
     while x:
         ret += x & 1
-        x >>= 1
+        x //= 2
     return ret
 
 def popcount_str(x: int) -> int:
@@ -27,12 +32,10 @@ def measure(f: Callable[[int], int], iteration: int = 10**5):
         _ = f(i)
     print(f"{f.__name__}:\t", time.time() - start)
 
-def measure_longint(f: Callable[[int], int], value: int = (1 << 100000) - 1):
-    start = time.time()
-    _ = f(value)
-    print(f"{f.__name__}:\t", time.time() - start)
-
-
 if __name__ == "__main__":
+    start = time.time()
+    make_bincountlist(10**5)
+    print(f"{make_bincountlist.__name__}:\t", time.time() - start)
+
     for f in [popcount_while, popcount_str, bit_count]:
-        measure_longint(f)
+        measure(f)
